cb923ba580fea3f4170ace13535c4601e975781f
Eric Santos <eric355402@yahoo.com>
2022-02-18 13:42:55 -0700

    fetched categories and featured image, no errors, console logging fetched data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,33 +1,32 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services';
 
-const posts = [
-  {title: "React Testing", excerpt: 'Learn React Testing'},
-  {title: "React with Tailwind", excerpt: 'Learn React Testing'}
-]
-
-export default function Home() {
+// destructured props coming from export at getStaticProps
+export default function Home({posts}) {
   return (
-    <div className="container mx-auto px-10 mb-8 ">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => (
-          <div>
-              <PostCard post={post} key={post.title}/>
-          </div>
-        ))}
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
-          <div className='lg:sticky relative top-8'>
+          <div className="lg:sticky relative top-8">
             <PostWidget />
-            <Categories/>
-            </div>
+            <Categories />
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
+}
+
+// nextjs way of requesting data instead of useEffect()
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props:{posts}
+  }
 }
